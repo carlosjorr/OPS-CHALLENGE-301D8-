1290e8deab725423682f1919bade8c1492ad6fa2
carlosj <carlosjorr@outlook.com>
2023-06-08 18:26:50 -0700

    yhis is my first commit

---
+++ 301challenge08.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#Script:                       Ops 301 Ops Chall 08
+# Author:                       carlos rojas 
+# Date of latest revision:      06/08/2023
+# Purpose:                      Python Collections 
+
+
+
+
+
+# Assigning a list of ten string elements to a variable
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "imbe", "jackfruit"]
+
+# Printing the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Printing the sixth through tenth element of the list
+print("Sixth to tenth elements:", my_list[5:])
+
+# Changing the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Printing the updated list
+print("Updated list:", my_list)
